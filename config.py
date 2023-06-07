80d80308555031a079a3a1d87c4d8fa6e19c4a73
JohnHwangzn <462902938@qq.com>
2023-06-07 13:41:01 +0800

    readme

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 # Settings
 os.environ["http_proxy"] = "http://127.0.0.1:7890"
 os.environ["https_proxy"] = "http://127.0.0.1:7890"
-api_key_list = ["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"] # 请填写你的 API KEY
+api_key_list = ["sk-xx"] # 请填写你的 API KEY
